a806e455dd7e861978e1db51f61946fb37f39fb7
Sarah Marshall <sr70079@gmail.com>
2021-01-07 11:30:43 -0500

    initial files

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+
+// variable and funtion to display current day at the top
+
+var currentDay = moment().format("dddd, MMMM Do");
+
+$("#currentDay").text(currentDay);
+
+// time block for each standard business hour
+
+    // color coded to show past, present, future time block 
+
+    // click into a time block 
+
+    //  enter event into time block
+
+    // click button to save event in time block 
+
+    // when button is clicked event is saved to local storage
+
+    // event persist is page is refreshed 
