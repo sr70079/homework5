1018cadce0b6f901e64cc84b771a3a43a3433d13
Sarah Marshall <sr70079@gmail.com>
2021-01-08 23:44:35 -0500

    added time color for past/present/future

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,8 @@ $(document).ready(function() {
 
     $("#currentDay").text(currentDay);
 
+    var now = moment().format("h a");
+
     // time block for each standard business hour
 
 
@@ -12,9 +14,7 @@ $(document).ready(function() {
     // var past = $(".past");
     // var present = $(".present");
     // var future = $(".future");    
-    // var textArea = $("<textarea></textarea>");
-    // var description = $(".description");
-
+    
       
     var workHours = [
             "9am",
@@ -34,36 +34,49 @@ $(document).ready(function() {
         for (var i = 0; i < workHours.length; i++) {      
           var timeBlockRow = $("<div>");
 
-          var timeBlockColHour = $("<div>")
-          var timeBlockColDescription = $("<div>")
-          var timeBlockColBtn = $("<button><i class='fa fa-save'></button>")
+          var timeBlockColHour = $("<div>");
+          var relativeTime = "";
+          var currentTime = moment(now, "h a");
+          var timeBlockTime = moment(workHours[i], "h a");
+          
+          if (currentTime.isBefore(timeBlockTime) === true) {
+           relativeTime = "future";
+        } else if (currentTime.isAfter(timeBlockTime) === true) {
+            relativeTime = "past";
+        } else {
+            relativeTime = "present";
+        }
 
-          var textArea = $("<textarea class='col-12'>")
+
+          var timeBlockColDescription = $("<div class='" + relativeTime + "'>");
+          var timeBlockColBtn = $("<button><i class='fa fa-save'></button>");
+
+          var textArea = $("<textarea class='col-12'>");
         //   var saveBtn = $("<button><i class='fa fa-save'></button>")
           
-          timeBlockColHour.addClass("hour col-2")
-          timeBlockColDescription.addClass("description col-8")
-          timeBlockColBtn.addClass("saveBtn col-2")
+          timeBlockColHour.addClass("hour col-2");
+          timeBlockColDescription.addClass("description col-8");
+          timeBlockColBtn.addClass("saveBtn col-2");
         //   saveBtn.addClass("saveBtn")
           timeBlockRow.addClass("row");
           timeBlockRow.attr("data-hours", workHours[i]);
           
           timeBlockColHour.text((workHours[i]));
-          timeBlockColDescription.append(textArea)
+          timeBlockColDescription.append(textArea);
         //   timeBlockColBtn.append(saveBtn)
 
-          timeBlock.append(timeBlockRow)
+          timeBlock.append(timeBlockRow);
 
-          timeBlockRow.append(timeBlockColHour)
-          timeBlockRow.append(timeBlockColDescription)
-          timeBlockRow.append(timeBlockColBtn)
+          timeBlockRow.append(timeBlockColHour);
+          timeBlockRow.append(timeBlockColDescription);
+          timeBlockRow.append(timeBlockColBtn);
 
 
         }
 
     $(".container").append(timeBlock);
 
-   
+
     
 
 });
