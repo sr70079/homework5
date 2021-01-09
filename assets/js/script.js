aee46ca5b7fad6247cc7852dd2a4cb66ed3f99ba
Sarah Marshall <sr70079@gmail.com>
2021-01-09 00:16:41 -0500

    onclick for save button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-// variable and funtion to display current day at the top
 
     var currentDay = moment().format('MMMM Do YYYY, h:mm a');
 
@@ -7,15 +6,6 @@ $(document).ready(function() {
 
     var now = moment().format("h a");
 
-    // time block for each standard business hour
-
-
-
-    // var past = $(".past");
-    // var present = $(".present");
-    // var future = $(".future");    
-    
-      
     var workHours = [
             "9am",
             "10am",
@@ -29,67 +19,63 @@ $(document).ready(function() {
         ];
 
     var timeBlock = $("<div class='time-block'>")
-
     
         for (var i = 0; i < workHours.length; i++) {      
-          var timeBlockRow = $("<div>");
+            var timeBlockRow = $("<div class='row'>");
+            var timeBlockColHour = $("<div class='hour col-2'>");
+            var relativeTime = "";
+            var currentTime = moment(now, "h a");
+            var timeBlockTime = moment(workHours[i], "h a");
+            
+            if (currentTime.isBefore(timeBlockTime) === true) {
+                relativeTime = "future";
+            } else if (currentTime.isAfter(timeBlockTime) === true) {
+                relativeTime = "past";
+            } else {
+                relativeTime = "present";
+            } 
 
-          var timeBlockColHour = $("<div>");
-          var relativeTime = "";
-          var currentTime = moment(now, "h a");
-          var timeBlockTime = moment(workHours[i], "h a");
-          
-          if (currentTime.isBefore(timeBlockTime) === true) {
-           relativeTime = "future";
-        } else if (currentTime.isAfter(timeBlockTime) === true) {
-            relativeTime = "past";
-        } else {
-            relativeTime = "present";
-        }
-
-
-          var timeBlockColDescription = $("<div class='" + relativeTime + "'>");
-          var timeBlockColBtn = $("<button><i class='fa fa-save'></button>");
-
-          var textArea = $("<textarea class='col-12'>");
-        //   var saveBtn = $("<button><i class='fa fa-save'></button>")
-          
-          timeBlockColHour.addClass("hour col-2");
-          timeBlockColDescription.addClass("description col-8");
-          timeBlockColBtn.addClass("saveBtn col-2");
-        //   saveBtn.addClass("saveBtn")
-          timeBlockRow.addClass("row");
-          timeBlockRow.attr("data-hours", workHours[i]);
-          
-          timeBlockColHour.text((workHours[i]));
-          timeBlockColDescription.append(textArea);
-        //   timeBlockColBtn.append(saveBtn)
-
-          timeBlock.append(timeBlockRow);
-
-          timeBlockRow.append(timeBlockColHour);
-          timeBlockRow.append(timeBlockColDescription);
-          timeBlockRow.append(timeBlockColBtn);
-
-
+            var timeBlockColDescription = $("<textarea class='description col-8 " + relativeTime + "'>");
+            var timeBlockColBtn = $("<button class='saveBtn col-2'><i class='fa fa-save'></button>");
+      
+            timeBlockRow.attr("data-hours", workHours[i]);            
+            timeBlockColHour.text((workHours[i]));
+            timeBlock.append(timeBlockRow);
+            timeBlockRow.append(timeBlockColHour);
+            timeBlockRow.append(timeBlockColDescription);
+            timeBlockRow.append(timeBlockColBtn);
         }
 
     $(".container").append(timeBlock);
 
+    $(".saveBtn").on("click", function() {
+        var saveTime = $(this).parent().attr("data-hours");
+        var saveText = $(this).siblings("textarea").val();
 
+        console.log(saveTime)
+        console.log(saveText)
+
+       
+    });
+
+    // function storeEvents() {
+
+    //     localStorage.setItem("events", JSON.stringify(workHours));
+    //   }
     
+    //   storeEvents();
 
 });
 
  
 
-    //     // color coded to show past, present, future time block 
+ 
 
-    //     // click into a time block 
 
-    //     //  enter event into time block
 
-    //     // click button to save event in time block 
+  
+
+  
 
     //     // when button is clicked event is saved to local storage
 
