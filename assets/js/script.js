564cfdd803d1175ae700279e02cd027679b95cbc
Sarah Marshall <sr70079@gmail.com>
2021-01-09 00:47:38 -0500

    save to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,16 +7,22 @@ $(document).ready(function() {
     var now = moment().format("h a");
 
     var workHours = [
-            "9am",
-            "10am",
-            "11am",
-            "12pm",
-            "1pm",
-            "2pm",
-            "3pm",
-            "4pm",
-            "5pm"
-        ];
+        { time: "9am", event: "" },
+        { time: "10am", event: "" },
+        { time: "11am", event: "" },
+        { time: "12pm", event: "" },
+        { time: "1pm", event: "" },
+        { time: "2pm", event: "" },
+        { time: "3pm", event: "" },
+        { time: "4pm", event: "" },
+        { time: "5pm", event: "" }
+    ];
+
+    var savedEvents = JSON.parse(localStorage.getItem("events"));
+
+    if (savedEvents !== null) {
+        workHours = savedEvents;
+    }
 
     var timeBlock = $("<div class='time-block'>")
     
@@ -25,7 +31,7 @@ $(document).ready(function() {
             var timeBlockColHour = $("<div class='hour col-2'>");
             var relativeTime = "";
             var currentTime = moment(now, "h a");
-            var timeBlockTime = moment(workHours[i], "h a");
+            var timeBlockTime = moment(workHours[i].time, "h a");
             
             if (currentTime.isBefore(timeBlockTime) === true) {
                 relativeTime = "future";
@@ -34,12 +40,12 @@ $(document).ready(function() {
             } else {
                 relativeTime = "present";
             } 
-
             var timeBlockColDescription = $("<textarea class='description col-8 " + relativeTime + "'>");
+            timeBlockColDescription.val(workHours[i].event);
             var timeBlockColBtn = $("<button class='saveBtn col-2'><i class='fa fa-save'></button>");
       
-            timeBlockRow.attr("data-hours", workHours[i]);            
-            timeBlockColHour.text((workHours[i]));
+            timeBlockRow.attr("data-hours", workHours[i].time);            
+            timeBlockColHour.text((workHours[i].time));
             timeBlock.append(timeBlockRow);
             timeBlockRow.append(timeBlockColHour);
             timeBlockRow.append(timeBlockColDescription);
@@ -52,19 +58,18 @@ $(document).ready(function() {
         var saveTime = $(this).parent().attr("data-hours");
         var saveText = $(this).siblings("textarea").val();
 
-        console.log(saveTime)
-        console.log(saveText)
-
-       
+        for (i= 0; i< workHours.length; i++) {
+            if (saveTime === workHours[i].time)
+            {
+                workHours[i].event = saveText;
+            }
+        }
+        storeEvents();       
     });
 
-    // function storeEvents() {
-
-    //     localStorage.setItem("events", JSON.stringify(workHours));
-    //   }
-    
-    //   storeEvents();
-
+    function storeEvents() {
+        localStorage.setItem("events", JSON.stringify(workHours));
+      }   
 });
 
  
